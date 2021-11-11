4f24f983c0eacd4ebd57bb27c5a22b9ac6696ff6
Tatiana Shuvainikova <shuvainikova.tp@phystech.edu>
2021-11-11 15:45:10 +0300

    Скрипт для вычисления калибровки и построения графика зависимости значения ацп от давления

--- kalibrovka.py
+++ kalibrovka.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 
 
 forty_array = np.loadtxt("40 mmHg.txt", dtype = int)
-sixty_array = np.loadtxt("60 mmHg.txt", dtype = int)
 eighty_array = np.loadtxt("80 mmHg.txt", dtype = int)
+hundred_twenty_array = np.loadtxt("120 mmHg.txt", dtype = int)
 hundred_sixty_array = np.loadtxt("160 mmHg.txt", dtype = int)
 
 f = np.mean(forty_array) #среднее значение АЦП при давлении 40 мм рт. столба
-s = np.mean(sixty_array)
 e = np.mean(eighty_array)
+ht = np.mean(hundred_twenty_array)
 hs = np.mean(hundred_sixty_array)
 
-kalibr_array = np.array([f, s, e, hs])
-pressure_array = np.array([40, 60, 80, 160])
+kalibr_array = np.array([f, e, ht, hs])
+pressure_array = np.array([40, 80, 120, 160])
 
 k = np.polyfit(kalibr_array, pressure_array, 1) #вычисление коэффициента колибровки
 with open('kalibrovka.txt', 'w') as f: #сохранение коэффициента колибровки в файл
